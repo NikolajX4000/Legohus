857bfa23212e13bebaad839ffb627948b04fd3ff
Nikolaj <supernikolaj@hotmail.com>
2018-03-19 10:43:55 +0100

    LoginSample

---
+++ mavenproject1/LegoHouse/src/main/java/DBAccess/Connector.java
@@ -0,0 +1,29 @@
+package DBAccess;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Connector {
+    private static final String IP = "159.89.9.144";
+    private static final int PORT = 3306;
+    public static final String DATABASE = "LegoHouse";
+    private static final String URL = "jdbc:mysql://" + IP + ":" + PORT + "/" + DATABASE;
+    private static final String USERNAME = "LegoHouse";
+    private static final String PASSWORD = "Password123";
+
+    private static Connection singleton;
+
+    public static void setConnection(Connection con) {
+        singleton = con;
+    }
+
+    public static Connection connection() throws ClassNotFoundException, SQLException {
+        if (singleton == null) {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            singleton = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        }
+        return singleton;
+    }
+
+}
