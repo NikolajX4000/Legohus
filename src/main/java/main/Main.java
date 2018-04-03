e2f55d4802a15e7b1f956489b38302b2bffefb5f
Nikolaj <supernikolaj@hotmail.com>
2018-04-03 08:34:08 +0200

    Finished begun styling

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -9,9 +9,12 @@ public class Main {
 
     public static void main(String[] args) {
         Random rand = new Random();
-        int length = rand.nextInt(10) + 8;
-        int width = rand.nextInt(10) + 6;
-        int height = rand.nextInt(10) + 7;
+        int length = rand.nextInt(20) + 8;
+//        int length = 8;
+        int width = rand.nextInt(20) + 5;
+//        int width = 6;
+        int height = rand.nextInt(20) + 7;
+//        int height = 7;
         House h = new House(length, width, height);
         System.out.println("length = " + length + ", width = " + width + ", height = " + height);
         for (Layer l : h.getHouse()) {
