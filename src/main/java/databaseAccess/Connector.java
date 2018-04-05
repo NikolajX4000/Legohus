db951fd090aa2339bfeb9acb6bf87dd5fa50c747
Nikolaj <supernikolaj@hotmail.com>
2018-04-05 09:32:36 +0200

    Finished begun styling

--- src/main/java/databaseAccess/Connector.java
+++ src/main/java/databaseAccess/Connector.java
@@ -9,7 +9,7 @@ public class Connector {
     private static final String IP = "159.89.9.144";
     private static final int PORT = 3306;
     public static final String DATABASE = "LegoHouse";
-    private static final String URL = "jdbc:mysql://" + IP + ":" + PORT + "/" + DATABASE + "?autoReconnect=true";
+    private static final String URL = "jdbc:mysql://" + IP + ":" + PORT + "/" + DATABASE + "?autoReconnect=true&useSSL=false";
     private static final String USERNAME = "LegoHouse";
     private static final String PASSWORD = "Password123";
 
